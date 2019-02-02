b2b3e4b2f7068051ab834dc476f86648be87efcb
James Begley <James@hp.outerheaven.lan>
2019-02-02 15:02:42 -0500

    comment cleanup in Node.h

--- Node.h
+++ Node.h
@@ -14,35 +14,6 @@
 
 #define MAXOBJS 32
 
-/*
- class Atom {
- public:
- Atom();
- Atom(std::string const& name);
- virtual ~Atom();
- static const char type;
- 
- std::string GetPath();
- char GetType();
- std::string identity;
- Atom* owner;
- 
- virtual Atom* Dereference(std::string const& name);
- virtual int Command(std::string const& cmd);
- virtual void SetValue(std::string const& val);
- virtual std::string GetValueAsStdString();
- virtual void Debug();
- virtual int OperatorEqu(Atom* atm); //  =
- virtual int OperatorAdd(Atom* atm); // +=
- virtual int OperatorSub(Atom* atm); // -=
- virtual int OperatorMul(Atom* atm); // *=
- virtual int OperatorDiv(Atom* atm); // /=
- virtual int OperatorPow(Atom* atm); // ^=
- protected:
- virtual int _Procedure();
- char _type;
- };
- */
 namespace jmb {
 	
 	namespace common {
