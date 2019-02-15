89b829478cfde3e7a88b65615d8c1c76490e1dcf
James <james@tyrfing10>
2019-02-15 03:54:18 -0500

    removed old xcode project, Makefile, build script; added autotools project; adopted name 'clab'

--- _Objects.mk
+++
@@ -1,2 +0,0 @@
-CLAB_OBJECTS = StrSplit.o Atom.o Node.o Notype.o String.o Integer.o Float.o
-CLAB_TEST_OBJECTS = main.o
--- build.sh
+++
@@ -1,2 +0,0 @@
-#clang++ *.cpp -Oz -o test
-g++ *.cpp -D_GLIBCXX_DEBUG -g3 -o test
